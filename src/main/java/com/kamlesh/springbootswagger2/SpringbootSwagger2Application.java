a8e947ac009c3477923829af1f7e2e82029af039
kamleshsilag <kamleshsilag42@gmail.com>
2020-07-15 17:46:59 +0530

    Application metadata

--- src/main/java/com/kamlesh/springbootswagger2/SpringbootSwagger2Application.java
+++ src/main/java/com/kamlesh/springbootswagger2/SpringbootSwagger2Application.java
@@ -5,9 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @SpringBootApplication
 @EnableSwagger2
@@ -23,7 +27,21 @@ public class SpringbootSwagger2Application {
                 .select()
                 .paths(PathSelectors.ant("/api/*"))
                 .apis(RequestHandlerSelectors.basePackage("com.kamlesh"))
-                .build();
+                .build()
+                .apiInfo(apiDetails());
+    }
+
+    private ApiInfo apiDetails(){
+        return new ApiInfo(
+                "Address book API",
+                "Sample API",
+                "0.1",
+                "Open Source",
+                new Contact("Kamlesh Silag","kamleshsilag.github.io","abc@gmail.com"),
+                "API Licence",
+                "kamleshsilag.github.io",
+                Collections.emptyList()
+        );
     }
 
 }
